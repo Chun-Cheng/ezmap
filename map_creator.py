8dd59514c5f4ee56fc27fa20ad74da1b1dec75a0
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2020-11-29 17:52:07 +0800

    Update map_creator.py

--- map_creator.py
+++ map_creator.py
@@ -234,7 +234,7 @@ def generate_map(data):
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width">
-        <title>repl.it</title>
+        <title>拓樸地圖</title>
         <link href="style.css" rel="stylesheet" type="text/css" />
     </head>
 
