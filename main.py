d9803202e01654a6bc8c2aa7f6f83ddb25eff32c
Chun-Cheng <61136202+Chun-Cheng@users.noreply.github.com>
2020-11-29 16:00:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,8 +29,10 @@ def allowed_file(filename):
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
+    # 刪除過期資料
+    datas.delete_many( { 'date' : { $lt : int(datetime.today().strftime('%Y%m%d'))-1 } } )
+    datas.delete_many( { 'time' : { $lt : int(datetime.now().strftime('%H%M%S'))-10000 } } )
     if request.method == 'POST':
-        
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('找不到檔案資料')
